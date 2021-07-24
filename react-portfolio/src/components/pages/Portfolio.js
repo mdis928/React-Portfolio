620096815965f4871132a703f1c7d634d2b8a365
Michael DiSanto <mdisanto928@gmail.com>
2021-07-24 09:55:40 -0400

    maxwidth property for pictures

--- react-portfolio/src/components/pages/Portfolio.js
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Blog() {
+export default function Portfolio() {
   return (
     <main>
       <h1>Portfolio</h1>
@@ -11,7 +11,7 @@ export default function Blog() {
             <ul>
               <li><a href="https://hidden-waters-49471.herokuapp.com/">Heroku Page</a></li>
               <li><a href="https://github.com/alexbradshaw/Wobal">Repository</a></li>
-              <img href="https://hidden-waters-49471.herokuapp.com/" src="Wobal2.PNG" style={{width: '95%', height: '95%'}}></img>
+              <img href="https://hidden-waters-49471.herokuapp.com/" src="Wobal2.PNG" style={{width: '95%', height: '95%', maxWidth: '800px'}}></img>
             </ul>
         </div>
       </div>
@@ -21,7 +21,7 @@ export default function Blog() {
             <ul>
               <li><a href="https://realzzkevin.github.io/Dinner-Meal-Planner/">Github Page</a></li>
               <li><a href="https://github.com/realzzkevin/team-project">Repository</a></li>
-              <img href="https://realzzkevin.github.io/Dinner-Meal-Planner/" src="Project1 - Meal Planning API.png" style={{width: '95%', height: '95%'}}></img>
+              <img href="https://realzzkevin.github.io/Dinner-Meal-Planner/" src="Project1 - Meal Planning API.png" style={{width: '95%', height: '95%', maxWidth: '800px'}}></img>
             </ul>
         </div>
       </div>
@@ -32,7 +32,7 @@ export default function Blog() {
             <ul>
               <li><a href="https://express-notetaker-mdis928.herokuapp.com/">Github Page</a></li>
               <li><a href="https://github.com/mdis928/Express-JS-Note-Taker">Repository</a></li>
-              <img href="https://express-notetaker-mdis928.herokuapp.com/" src="Note Taker.PNG" style={{width: '95%', height: '95%'}}></img>
+              <img href="https://express-notetaker-mdis928.herokuapp.com/" src="Note Taker.PNG" style={{width: '95%', height: '95%', maxWidth: '800px'}}></img>
             </ul>
         </div>
       </div>
@@ -42,7 +42,7 @@ export default function Blog() {
             <ul>
               <li><a href="https://mdis928.github.io/Third-Party-APIs/">Heroku Page</a></li>
               <li><a href="https://github.com/mdis928/Third-Party-APIs">Repository</a></li>
-              <img href="https://mdis928.github.io/Third-Party-APIs/" src="Work Day Scheduler .png" style={{width: '95%', height: '95%'}}></img>
+              <img href="https://mdis928.github.io/Third-Party-APIs/" src="Work Day Scheduler .png" style={{width: '95%', height: '95%', maxWidth: '800px'}}></img>
             </ul>
         </div>
       </div>
@@ -52,7 +52,7 @@ export default function Blog() {
             <ul>
               <li><a href="https://github.com/mdis928/MySQL-Employee-Tracker">Github Page</a></li>
               <li><a href="https://drive.google.com/file/d/1PIKLCwjRgkt1yIn2wK8_StS8pBACaveW/view">Repository</a></li>
-              <img href="https://github.com/mdis928/MySQL-Employee-Tracker" src="Employee Tracker.PNG" style={{width: '95%', height: '95%'}}></img>
+              <img href="https://github.com/mdis928/MySQL-Employee-Tracker" src="Employee Tracker.PNG" style={{width: '95%', height: '95%', maxWidth: '800px'}}></img>
             </ul>
         </div>
       </div>
